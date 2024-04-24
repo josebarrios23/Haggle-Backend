0358afd485f8e975b0e9a606925685af4a99554e
Jose Barrios <josebarrios@pursuit.org>
2024-04-24 14:56:20 -0400

    changed .env file

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,16 +1,16 @@
 const pgp = require('pg-promise')()
 require('dotenv').config()
 
-const cn = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-}
-// const connectionstring = process.env.CONNECTION_STRING
-// const db = pgp(connectionstring)
+// const cn = {
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   database: process.env.PG_DATABASE,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+// }
+const connectionstring = process.env.CONNECTION_STRING
+const db = pgp(connectionstring)
 
-const db = pgp(cn)
+// const db = pgp(cn)
 
 module.exports = db
